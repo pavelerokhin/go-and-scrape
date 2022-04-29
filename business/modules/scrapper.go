92526a622d9308b2f818cab33485e7f11917a52e
pavelerokhin <pavel.erokhin@rstor.io>
2022-04-29 20:07:47 +0200

    persist valid url

--- business/modules/scrapper.go
+++ business/modules/scrapper.go
@@ -19,7 +19,7 @@ func NewScrapper(logger *log.Logger) *Scrapper {
 	return &Scrapper{logger: logger}
 }
 
-// ScrapMedium is a function that scpapa medium and returns a slice of Article
+// ScrapMedium is a function that scraps medium and returns a slice of Article
 // objects and error
 func (s *Scrapper) ScrapMedium(mediumConfig *configs.MediumConfig) []entities.Article {
 	s.logger.Printf("start to scrap medium %s", mediumConfig.Name)
@@ -56,7 +56,8 @@ func (s *Scrapper) ScrapMedium(mediumConfig *configs.MediumConfig) []entities.Ar
 		title := strings.TrimSpace(item.Find(mediumConfig.HTMLTags.Title).Text())
 		subtitle := strings.TrimSpace(item.Find(mediumConfig.HTMLTags.Subtitle).Text())
 		urlArticle, _ := item.Find(mediumConfig.HTMLTags.URL).Attr("href")
-		urlArticle = fmt.Sprintf("%s%s", mediumConfig.URL, urlArticle)
+
+		urlArticle = s.getUrl(mediumConfig.URL, urlArticle)
 
 		articles = append(articles, entities.Article{
 			Tag:      tag,
@@ -68,4 +69,79 @@ func (s *Scrapper) ScrapMedium(mediumConfig *configs.MediumConfig) []entities.Ar
 
 	s.logger.Printf("scrapping of medium %s finished successfully", mediumConfig.Name)
 	return articles
+}
+
+func (s *Scrapper) getUrl(mediumUrl, articleUrl string) string {
+	var fullUrl string
+	if isUrlValid(articleUrl) {
+		return articleUrl
+	}
+
+	if strings.HasSuffix(mediumUrl, "/") &&
+		strings.HasPrefix(articleUrl, "/") {
+		fullUrl = fmt.Sprintf("%s%s", mediumUrl, articleUrl[1:])
+		if isUrlValid(fullUrl) {
+			return fullUrl
+		}
+	}
+
+	if !strings.HasSuffix(mediumUrl, "/") &&
+		!strings.HasPrefix(articleUrl, "/") {
+		fullUrl = fmt.Sprintf("%s/%s", mediumUrl, articleUrl)
+		if isUrlValid(fullUrl) {
+			return fullUrl
+		}
+	}
+
+	fullUrl = fmt.Sprintf("%s%s", mediumUrl, articleUrl)
+	if isUrlValid(fullUrl) {
+		return fullUrl
+	}
+
+	urlWithoutDuplications := tryRemoveDuplicates(fullUrl)
+	if urlWithoutDuplications == fullUrl || !isUrlValid(urlWithoutDuplications) {
+		s.logger.Printf("failed to find the correct URL for the article. Medium URL (%s), article URL (%s)",
+			mediumUrl, articleUrl)
+		return ""
+	}
+	return urlWithoutDuplications
+
+}
+
+func isUrlValid(url string) bool {
+	resp, err := http.Get(url)
+	return err == nil && (resp.StatusCode >= 200 && resp.StatusCode <= 299)
+}
+
+func tryRemoveDuplicates(url string) string {
+	var newURL string
+	parts := deleteEmpty(strings.Split(url, "/"))
+
+	newURL = parts[0]
+	if strings.HasPrefix(newURL, "http") {
+		newURL += "/"
+	}
+
+	for i, part := range parts {
+		if i == 0 {
+			continue
+		}
+		if part == parts[i-1] {
+			continue
+		}
+
+		newURL = fmt.Sprintf("%s/%s", newURL, part)
+	}
+
+	return newURL
+}
+
+func deleteEmpty(s []string) []string {
+	var r []string
+	for _, str := range s {
+		if str != "" {
+			r = append(r, str)
+		}
+	}
+	return r
 }
