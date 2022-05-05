6cc41361fb6ebf654480fda9aaa2f2d1f1b0a11a
pavelerokhin <pavel.erokhin@rstor.io>
2022-05-05 14:34:12 +0200

    work in progress

--- business/nlp/ner.py
+++ business/nlp/ner.py
@@ -1,9 +1,13 @@
 import spacy
+
+import cleaning as c
 import sqliteconnector as s
 
 
 nlp = spacy.load('ru_core_news_sm')
 for text in s.get_texts("mediums"):
-    doc = nlp(text)
+    t = c.spoil_ebala(text)
+    print(t[:100], "...")
+    doc = nlp(t)
     for ent in doc.ents:
         print(ent)
