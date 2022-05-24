bdf54246e3de74db97395a036d78f492bb596eb5
pavelerokhin <pavel.erokhin@rstor.io>
2022-05-24 20:58:34 +0200

    card template

--- gui/cli/newsTemplate.go
+++ gui/cli/newsTemplate.go
@@ -1,17 +1,55 @@
 package cli
 
-import "github.com/charmbracelet/lipgloss"
+import (
+	"github.com/charmbracelet/lipgloss"
+	"github.com/pavelerokhin/go-and-scrape/models/entities"
+)
 
 var (
 	highlight   = lipgloss.AdaptiveColor{Light: "#874BFD", Dark: "#7D56F4"}
-	columnWidth = 35
+	columnWidth = 40
 
-	historyStyle = lipgloss.NewStyle().
+	border = lipgloss.Border{
+		Top:         "─",
+		Bottom:      "─",
+		Left:        "│",
+		Right:       "│",
+		TopLeft:     "╭",
+		TopRight:    "╮",
+		BottomLeft:  "╰",
+		BottomRight: "╯",
+	}
+
+	card = lipgloss.NewStyle().
+		Border(border, true).
+		BorderForeground(lipgloss.Color("#f50202")).
+		Padding(0, 1)
+
+	cardActive = lipgloss.NewStyle().
+			Border(border, true).
+			BorderForeground(highlight).
+			Padding(0, 1)
+
+	titleInnerStyle = lipgloss.NewStyle().
 			Align(lipgloss.Left).
-			Foreground(lipgloss.Color("#FAFAFA")).
-			Background(highlight).
+			Foreground(lipgloss.Color("#fafafa")).
 			Margin(1, 3, 0, 0).
 			Padding(1, 2).
-			Height(7).
+			Height(4).
 			Width(columnWidth)
+
+	subtitleInnerStyle = lipgloss.NewStyle().
+				Align(lipgloss.Left).
+				Foreground(lipgloss.Color("#aaaaaa")).
+				Margin(1, 3, 0, 0).
+				Padding(1, 2).
+				Height(2).
+				Width(columnWidth)
 )
+
+func MakeCard(articlePreview entities.ArticlePreview, isActive bool) string {
+	if isActive {
+		return card.Render(lipgloss.JoinVertical(lipgloss.Left, titleInnerStyle.Render(articlePreview.Title), subtitleInnerStyle.Render(articlePreview.Subtitle)))
+	}
+	return cardActive.Render(lipgloss.JoinVertical(lipgloss.Left, titleInnerStyle.Render(articlePreview.Title), subtitleInnerStyle.Render(articlePreview.Subtitle)))
+}
